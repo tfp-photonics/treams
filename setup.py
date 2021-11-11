db45964e9904ab27e2855eddd390bb2ccd344119
Dominik Beutel <dominik.beutel@kit.edu>
2021-11-11 18:23:13 +0100

    Add coverage option for cython

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 """Packaging of ptsa"""
+import os
 from setuptools import setup, Extension
 import numpy as np
 from Cython.Build import cythonize
 
-# For linetracing:
-# from Cython.Compiler.Options import get_directive_defaults
-# directive_defaults = get_directive_defaults()
-# directive_defaults['linetrace'] = True
+keys = {"include_dirs": [np.get_include()]}
+compiler_directives = {}
+if os.environ.get("CYTHON_COVERAGE", False):
+    keys["define_macros"] = [("CYTHON_TRACE_NOGIL", "1")]
+    compiler_directives["linetrace"] = True
 
-extensions = [
-    Extension(
-        "*",
-        ["ptsa/{,lattice/,special/}*.pyx"],
-        include_dirs=[np.get_include()],
-        # define_macros=[('CYTHON_TRACE_NOGIL', '1')],  # for linetracing
-    )
-]
+extensions = [Extension("*", ["ptsa/{,lattice/,special/}*.pyx"], **keys,)]
 setup(
     name="ptsa",
     version="0.1",
     packages=["ptsa", "ptsa.special", "ptsa.lattice"],
     install_requires=["numpy", "scipy>=1.6", "cython"],
     extras_require={"test": ["pytest", "pytest-cov"], "docs": ["sphinx"]},
-    ext_modules=cythonize(extensions, language_level="3"),
+    ext_modules=cythonize(
+        extensions, language_level="3", compiler_directives=compiler_directives
+    ),
     package_data={
         "ptsa/lattice": ["cython_lattice.pxd"],
         "ptsa/special": ["cython_special.pxd"],
