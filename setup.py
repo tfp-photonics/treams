35466608d75b774677df527629423e1a48330ec0
Dominik Beutel <dominik.beutel@kit.edu>
2021-12-02 19:06:49 +0100

    Packaging of ptsa for Linux and Windows

--- setup.py
+++ setup.py
@@ -1,27 +1,94 @@
 """Packaging of ptsa"""
 import os
-from setuptools import setup, Extension
+
 import numpy as np
-from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+from setuptools.command.build_ext import build_ext as build_ext_orig
+
+if os.name == "nt":
+
+    class build_ext(build_ext_orig):
+        def finalize_options(self):
+            super().finalize_options()
+            self.compiler = "mingw32"
+
+
+else:
+    build_ext = build_ext_orig
+
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
 
 keys = {"include_dirs": [np.get_include()]}
-compiler_directives = {}
+compiler_directives = {"language_level": "3"}
 if os.environ.get("CYTHON_COVERAGE", False):
     keys["define_macros"] = [("CYTHON_TRACE_NOGIL", "1")]
     compiler_directives["linetrace"] = True
 
-extensions = [Extension("*", ["ptsa/{,lattice/,special/}*.pyx"], **keys,)]
+extension_names = [
+    "ptsa.coeffs",
+    "ptsa.cw",
+    "ptsa.pw",
+    "ptsa.sw",
+    "ptsa.lattice._dsum",
+    "ptsa.lattice._esum",
+    "ptsa.lattice._gufuncs",
+    "ptsa.lattice._misc",
+    "ptsa.lattice.cython_lattice",
+    "ptsa.special._bessel",
+    "ptsa.special._coord",
+    "ptsa.special._gufuncs",
+    "ptsa.special._integrals",
+    "ptsa.special._misc",
+    "ptsa.special._ufuncs",
+    "ptsa.special._waves",
+    "ptsa.special._wigner3j",
+    "ptsa.special._wignerd",
+    "ptsa.special.cython_special",
+]
+extensions = [
+    Extension(name, [f"{name.replace('.', '/')}.pyx"], **keys,)
+    for name in extension_names
+]
+
+if cythonize is not None:
+    try:
+        extensions = cythonize(extensions, compiler_directives=compiler_directives)
+    except ValueError:
+        extensions = no_cythonize(extensions)
+else:
+    extensions = no_cythonize(extensions)
+
 setup(
     name="ptsa",
     version="0.1",
     packages=["ptsa", "ptsa.special", "ptsa.lattice"],
-    install_requires=["numpy", "scipy>=1.6", "cython"],
+    install_requires=["numpy", "scipy>=1.6"],
     extras_require={"test": ["pytest", "pytest-cov"], "docs": ["sphinx"]},
-    ext_modules=cythonize(
-        extensions, language_level="3", compiler_directives=compiler_directives
-    ),
+    ext_modules=extensions,
     package_data={
-        "ptsa/lattice": ["cython_lattice.pxd"],
-        "ptsa/special": ["cython_special.pxd"],
+        "ptsa.lattice": ["ptsa/lattice/cython_lattice.pxd"],
+        "ptsa.special": ["ptsa/special/cython_special.pxd"],
     },
+    cmdclass={"build_ext": build_ext},
 )
