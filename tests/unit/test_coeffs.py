fc8e35c7f984242e4e2dcea17997e51b9ae296a0
Dominik Beutel <dominik.beutel@kit.edu>
2022-01-13 17:56:54 +0100

    Add test for coefficients

--- tests/unit/test_coeffs.py
+++ tests/unit/test_coeffs.py
@@ -14,13 +14,13 @@ class TestMie:
             [-0.207488775205865 - 0.398855023857451j, 0.047334571416857 + 0.044794841521540j],
             [0.059646659631622 + 0.056446326342700j, -0.117865783454384 - 0.314040741356843j],
         ])
-        assert np.sum(np.abs(cf.mie(1, [1, 2, 4], [1, 1.3, 3, 2], [1, 2, 1, 1.5], [.1, 0, .2, .1]) - expect)) < EPSSQ
+        assert np.all(np.abs(cf.mie(1, [1, 2, 4], [1, 1.3, 3, 2], [1, 2, 1, 1.5], [.1, 0, .2, .1]) - expect) < EPSSQ)
     def test_complex(self):
         expect = np.array([
             [-0.233064414843298 + 0.218720052849983j, 0.047852376140501 + 0.048918498716278j],
             [0.084573700327288 + 0.086457952239276j, -0.619693739232935 + 0.081746714390217j],
         ])
-        assert np.sum(np.abs(cf.mie(3, [1, 2, 3], [1, 2, 3 + 1j, 4], [4, 3 + .1j, 1, 2], [.3, -.1 + .1j, 1 + .1j, .4]) - expect)) < EPSSQ
+        assert np.all(np.abs(cf.mie(3, [1, 2, 3], [1, 2, 3 + 1j, 4], [4, 3 + .1j, 1, 2], [.3, -.1 + .1j, 1 + .1j, .4]) - expect) < EPSSQ)
 
 
 class TestFresnel:
@@ -47,11 +47,11 @@ class TestFresnel:
                 ],
             ],
         ]
-        assert np.sum(np.abs(cf.fresnel(
+        assert np.all(np.abs(cf.fresnel(
             [[3, 5], [2, 4]],
             [[np.sqrt(8), np.sqrt(24)], [np.sqrt(3), np.sqrt(15)]],
             [1, 1],
-        ) - expect)) < EPSSQ
+        ) - expect) < EPSSQ)
     def test_evanescent(self):
         expect = [
             [
@@ -75,11 +75,11 @@ class TestFresnel:
                 ],
             ],
         ]
-        assert np.sum(np.abs(cf.fresnel(
+        assert np.all(np.abs(cf.fresnel(
             [[3, 5], [3, 3]],
             [[1j * np.sqrt(7), 3], [1j * np.sqrt(7), 1j * np.sqrt(7)]],
             [1 / 4, 1],
-        ) - expect)) < EPSSQ
+        ) - expect) < EPSSQ)
     def test_complex(self):
         expect = [
             [
@@ -109,13 +109,51 @@ class TestFresnel:
         ks = np.stack((np.sqrt(epsilon * mu) - kappa, np.sqrt(epsilon * mu) + kappa), axis=-1)
         kzs = np.sqrt(ks * ks - 1)
         zs = np.sqrt(mu / epsilon)
-        assert np.sum(np.abs(cf.fresnel(ks, kzs, zs) - expect)) < EPSSQ
+        assert np.all(np.abs(cf.fresnel(ks, kzs, zs) - expect) < EPSSQ)
 
 
 class TestMieCyl:
     def test_real(self):
-        assert 0
+        # Calculated with comsol
+        expect = [
+            [-0.87100-0.069690j, -0.14510-0.2492j],
+            [-0.18286-0.30517j, -0.62281+0.37137j],
+        ]
+        m = -2
+        epsilon = [1, 4, 2]
+        mu = [4, 3, 2]
+        kappa = [-0.5, .1, .2]
+        kz = 2 * np.pi / 600
+        k0 = 2 * np.pi / 500
+        radii = [50, 100]
+        assert np.all(np.abs(cf.mie_cyl(kz, m, k0, radii, epsilon, mu, kappa) - expect) / np.max(np.abs(expect)) < 1.1e-2)
+
     def test_evanescent(self):
-        assert 0
+        # Calculated with comsol
+        expect = [
+            [-3.2138e-4-29.992j, -0.0031377+43.380j],
+            [6.7389e-4+35.637j, -0.0090938-74.594j],
+        ]
+        m = 1
+        epsilon = [3, 2, 2]
+        mu = [6, 1, 2]
+        kappa = [1, 0, -.2]
+        kz = 2 * np.pi / 150
+        k0 = 2 * np.pi / 500
+        radii = [30, 100]
+        assert np.all(np.abs(cf.mie_cyl(kz, m, k0, radii, epsilon, mu, kappa) - expect) / np.max(np.abs(expect)) < 1e-2)
+
     def test_complex(self):
-        assert 0
+        # Calculated with comsol
+        expect = [
+            [-0.88746-0.15313j, 0.12955+0.086434j],
+            [0.103112+0.081574j, -0.098055+0.017689j],
+        ]
+        m = -1
+        epsilon = [4, 2 + 1j, 2 + .02j]
+        mu = [1 + 0.5j, 1, 2 + 0.1j]
+        kappa = [-0.5, .1 + .1j, -.2 + 0.001j]
+        kz = 2 * np.pi / 250
+        k0 = 2 * np.pi / 500
+        radii = [50, 100]
+        assert np.all(np.abs(cf.mie_cyl(kz, m, k0, radii, epsilon, mu, kappa) - expect) / np.max(np.abs(expect)) < 1e-2)
