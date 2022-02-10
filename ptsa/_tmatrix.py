Fixes in the cross section/width calculation

ptsa/_tmatrix.py
@@ -190,13 +190,11 @@ class TMatrix(TMatrixBase):
         if not self.helicity:
             raise NotImplementedError
         selections = self.pol == 0, self.pol == 1
-        tpp = self.t[np.ix_(selections[1], selections[1])]
-        tpm = self.t[np.ix_(selections[1], selections[0])]
-        tmp = self.t[np.ix_(selections[0], selections[1])]
-        tmm = self.t[np.ix_(selections[0], selections[0])]
-        plus = tpp + tpp.T.conj() + 2 * tpp.T.conj() @ tpp + 2 * tmp.T.conj() @ tmp
-        minus = tmm + tmm.T.conj() + 2 * tmm.T.conj() @ tmm + 2 * tpm.T.conj() @ tpm
-        return 2 * np.pi * np.real(np.trace(minus) - np.trace(plus))
+        cd = -np.sum(np.real(self.t[selections[1], selections[1]]))
+        cd -= np.sum(np.power(np.abs(self.t[:, selections[1]]), 2))
+        cd += np.sum(np.real(self.t[selections[0], selections[0]]))
+        cd += np.sum(np.power(np.abs(self.t[:, selections[0]]), 2))
+        return 4 * np.pi * cd
 
     @property
     def chi(self):

ptsa/_tmatrixc.py
@@ -186,14 +186,15 @@ class TMatrixC(TMatrixBase):
         """
         if self.positions.shape[0] > 1:
             raise NotImplementedError
+        nk = np.unique(self.kz).size
         if self.kappa == 0:
-            res = -2 * np.real(np.trace(self.t)) / self.ks[0]
+            res = -2 * np.real(np.trace(self.t)) / (self.ks[0] * nk)
         else:
             res = 0
             for pol in [0, 1]:
                 choice = self.pol == pol
                 res += (
-                    -2 * np.real(np.trace(self.t[choice, :][:, choice])) / self.ks[pol]
+                    -2 * np.real(np.trace(self.t[choice, :][:, choice])) / (self.ks[pol] * nk)
                 )
         if res.imag == 0:
             return res.real
@@ -211,16 +212,17 @@ class TMatrixC(TMatrixBase):
         """
         if self.positions.shape[0] > 1:
             raise NotImplementedError
+        nk = np.unique(self.kz).size
         if self.kappa == 0:
             res = (
                 2
                 * np.sum(self.t.real * self.t.real + self.t.imag * self.t.imag)
-                / self.ks[0]
+                / (self.ks[0] * nk)
             )
         else:
             res = 2 * np.sum(
                 (self.t.real * self.t.real + self.t.imag * self.t.imag)
-                / self.ks[self.pol, None]
+                / (self.ks[self.pol, None] * nk)
             )
         if res.imag == 0:
             return res.real