FIX: Average CD calculation embedded in chiral media

ptsa/_tmatrix.py
@@ -204,10 +204,10 @@ class TMatrix(TMatrixBase):
         selections = self.pol == 0, self.pol == 1
         plus = -np.sum(np.real(self.t[selections[1], selections[1]]))
         plus -= np.sum(np.power(np.abs(self.t[:, selections[1]]), 2))
-        plus /= self.ks[1]
+        plus /= self.ks[1] * self.ks[1]
         minus = -np.sum(np.real(self.t[selections[0], selections[0]]))
         minus -= np.sum(np.power(np.abs(self.t[:, selections[0]]), 2))
-        minus /= self.ks[0]
+        minus /= self.ks[0] * self.ks[0]
         return np.real((plus - minus) / (plus + minus))
 
     @property