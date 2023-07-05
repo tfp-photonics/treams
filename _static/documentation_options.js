046f7a5190394f4c9b79779f070a557685665e28
dbeutel <dbeutel@users.noreply.github.com>
2023-07-05 11:29:46 +0000

    deploy: 13756fecedbbe2e06db8f953859250063c830e93

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1.dev22+g1056b14b',
+    VERSION: '0.3.1.dev24+g13756fec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
